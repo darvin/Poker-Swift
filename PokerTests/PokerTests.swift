f14c257ab14093ab8bc2c9e3a9ee7f15290f8602
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-04-22 23:08:06 -0700

    Initial Commit

---
+++ PokerTests/PokerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PokerTests.swift
+//  PokerTests
+//
+//  Created by Sergey Klimov on 4/22/15.
+//  Copyright (c) 2015 Sergey Klimov. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PokerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
